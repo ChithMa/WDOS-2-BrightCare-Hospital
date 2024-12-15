a811c1546bcd3ae03c7f2a92f295b4ff29e83df1
Chithma Pathirana <162292641+ChithMa@users.noreply.github.com>
2024-12-16 00:54:31 +0530

    Update pharnacy.js

--- scripts/pharnacy.js
+++ scripts/pharnacy.js
@@ -216,7 +216,7 @@ async function printStuff(){
 
                 <div class="quantity-selector">
                     <form>
-                    
+                    <p>Quantity:</p>
                     <input type="number" id="quantity-${i}" class="quantity-input" value="1" min="1">
                     </form>
                 </div>
